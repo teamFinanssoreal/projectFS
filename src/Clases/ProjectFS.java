2ea8ce8ce4aa7596e7d235c34aec262b989beca5
Geison <Geison@192.168.56.1>
2020-11-30 11:26:12 -0600

    Inicio del proyecto

---
+++ src/Clases/ProjectFS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+import Formularios.frmPrincipal;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Geison
+ */
+
+public class ProjectFS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal Principal = new frmPrincipal();
+        Principal.setLocationRelativeTo(null);
+        Principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        Principal.setVisible(true);
+              
+    }
+    
+}
