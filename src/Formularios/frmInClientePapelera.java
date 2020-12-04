275a1a39b08af52a1d852b5f1c23614e26920f74
alberto <alber@LAPTOP-VLIS2FKF>
2020-12-04 14:46:12 -0600

    Mejora de Íconos y Comentado de Código

--- src/Formularios/frmInClientePapelera.java
+++ src/Formularios/frmInClientePapelera.java
@@ -29,9 +29,9 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        txtBuscarPorDPI = new javax.swing.JTextField();
+        txtPapeleraBuscarPorDPI = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        txtBuscarPorNombre = new javax.swing.JTextField();
+        txtPapeleraBuscarPorNombre = new javax.swing.JTextField();
         lblBotonBuscarCliente = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         lblBotonRestaurarCliente = new javax.swing.JLabel();
@@ -39,7 +39,7 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
         jPanel3 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tbPapeleraClientes = new javax.swing.JTable();
         lblBotonMoverFinal = new javax.swing.JLabel();
         lblBotonMoverAdelante = new javax.swing.JLabel();
         lblBotonMoverAtras = new javax.swing.JLabel();
@@ -69,8 +69,8 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel3)
                     .addComponent(jLabel1)
-                    .addComponent(txtBuscarPorDPI, javax.swing.GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
-                    .addComponent(txtBuscarPorNombre))
+                    .addComponent(txtPapeleraBuscarPorDPI, javax.swing.GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
+                    .addComponent(txtPapeleraBuscarPorNombre))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                 .addComponent(lblBotonBuscarCliente)
                 .addGap(21, 21, 21))
@@ -83,11 +83,11 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblBotonBuscarCliente)
-                    .addComponent(txtBuscarPorDPI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtPapeleraBuscarPorDPI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(42, 42, 42)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtBuscarPorNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtPapeleraBuscarPorNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -102,7 +102,7 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
         });
 
         jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel7.setText("PAPELERA");
+        jLabel7.setText("RESTAURAR REGISTRO");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -110,17 +110,19 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(39, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblBotonRestaurarCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(lblBotonRestaurarCliente)))
                 .addGap(39, 39, 39))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(44, Short.MAX_VALUE)
+                .addContainerGap(36, Short.MAX_VALUE)
                 .addComponent(lblBotonRestaurarCliente)
-                .addGap(18, 18, 18)
+                .addGap(26, 26, 26)
                 .addComponent(jLabel7)
                 .addGap(27, 27, 27))
         );
@@ -138,14 +140,14 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(jLabel2)
-                .addContainerGap(45, Short.MAX_VALUE))
+                .addContainerGap(61, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tbPapeleraClientes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null},
                 {null, null, null, null, null},
@@ -156,7 +158,7 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
                 "CÓDIGO", "DPI", "NOMBRE DEL CLIENTE", "TELÉFONO", "CORREO ELECTRÓNICO"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tbPapeleraClientes);
 
         lblBotonMoverFinal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/crud_paginacion_limit_right_64x64.png"))); // NOI18N
 
@@ -187,7 +189,7 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
                         .addComponent(lblBotonMoverAdelante)
                         .addGap(18, 18, 18)
                         .addComponent(lblBotonMoverFinal)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -226,14 +228,14 @@ public class frmInClientePapelera extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JLabel lblBotonBuscarCliente;
     private javax.swing.JLabel lblBotonMoverAdelante;
     private javax.swing.JLabel lblBotonMoverAtras;
     private javax.swing.JLabel lblBotonMoverFinal;
     private javax.swing.JLabel lblBotonMoverInicio;
     private javax.swing.JLabel lblBotonRestaurarCliente;
-    private javax.swing.JTextField txtBuscarPorDPI;
-    private javax.swing.JTextField txtBuscarPorNombre;
+    private javax.swing.JTable tbPapeleraClientes;
+    private javax.swing.JTextField txtPapeleraBuscarPorDPI;
+    private javax.swing.JTextField txtPapeleraBuscarPorNombre;
     // End of variables declaration//GEN-END:variables
 }
