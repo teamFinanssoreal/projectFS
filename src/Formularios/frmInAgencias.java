c4421ad7dee8790024dfcd74c4120e2c57ed9c5e
Martin Rosales <Martin Rosales@Usuario7>
2020-12-14 19:14:40 -0600

    Cambio de Reestructuracion de ConexionBD

--- src/Formularios/frmInAgencias.java
+++ src/Formularios/frmInAgencias.java
@@ -24,6 +24,9 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+
+//
+
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
